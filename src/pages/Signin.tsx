a715e646c793c0dd5005dc47cc052268795dcb09
ha4219 <ha4219@naver.com>
2023-04-05 23:52:15 +0900

    Assignment 1 Fixes #1

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -30,8 +30,6 @@ function SigninPage() {
 
   const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    console.log(e.target.value);
-
     setIsValid((prev) => ({
       ...prev,
       password: validatePassword(e.target.value),
@@ -60,7 +58,11 @@ function SigninPage() {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
         </div>
-        <button disabled={isValid.email && isValid.password} type="submit">
+        <button
+          disabled={!(isValid.email && isValid.password)}
+          type="submit"
+          className="bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:focus:ring-blue-800 disabled:bg-gray-700"
+        >
           submit
         </button>
       </form>
